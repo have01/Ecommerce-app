85a74829cd92f86358d6d7c68e769f8d8249e8ea
Amrit-Raj12 <amrit.1224@gmail.com>
2023-04-06 14:58:31 +0530

    Dev - fix merge conflicts

--- src/components/meta.js
+++ src/components/meta.js
@@ -2,21 +2,11 @@ import Head from "next/head";
 
 const Meta = ({ title, description, children }) => {
   return (
-<<<<<<< Updated upstream
-    <Head>
-      <title>{title ? title : "Ecommerce App"}</title>
-      <meta name="description" content={description} />
-      <meta charset="utf-8" />
-      {/* <link rel="icon" href="%PUBLIC_URL%/favicon.ico" /> */}
-
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-=======
     <>
       <Head>
         <title>{title ? title : "Ecommerce App"}</title>
         <meta name="description" content={description} />
       </Head>
->>>>>>> Stashed changes
       {children}
     </>
   );
