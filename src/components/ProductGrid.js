1eec049a1a2616ef1d2e0bb7b022209ed947dd0c
Amrit-Raj12 <amrit.1224@gmail.com>
2023-04-09 19:57:08 +0530

    Dev - modified categaory dropdown

---
+++ src/components/ProductGrid.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import ProductCard from '../../components/productCard'
+
+const ProductGrid = () => {
+  return (
+    <div className='container mx-auto w-screen'>
+    <div className='min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 space-x-5  lg:grid-cols-4 max-w-6xl mx-auto p-2'>
+
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+
+
+    </div>
+</div>
+  )
+}
+
+export default ProductGrid
