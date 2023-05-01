02844cf177ac5942ce680d0c2a898831fb3fb6bc
harshkumar1302 <bs824536@gmail.com>
2023-05-01 14:16:02 +0530

    paymentSuccessful page

--- src/pages/paymentSuccessful.js
+++ src/pages/paymentSuccessful.js
@@ -17,7 +17,7 @@ const PaymentSuccessful = () => {
                         <p> Have a great day!  </p>
                         <div class="py-10 text-center">
                             <Link href="/" class="px-12 bg-indigo-600 hover:bg-indigo-500 text-white font-semibold py-3">
-                                GO BACK
+                                Continue Shopping
                             </Link>
                         </div>
                     </div>
