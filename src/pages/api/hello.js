0fd0ddf0f84b1c986a8a5dba46b8638cc6553b3d
Dheerendra Kumar <53529978+have01@users.noreply.github.com>
2023-05-01 19:59:41 +0530

    Update hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,4 +1,5 @@
 export default function handler(req, res) {
-  res.redirect('https://www.google.com')
+ res.redirect('https://checkout.stripe.com/c/pay/cs_test_a1ZzeyGg2N6KvQKuxx6GWG2fGHtHG9ZIxCmceZUtqqgBC2PVQAy5D44KLB#fidkdWxOYHwnPyd1blpxYHZxWjA0SzR2UV1WQkl%2FTndNQDxzPTZiTWtHYnYwamY0UjE8MjR9MGZQYEltcUdTPHNidlUxY0txTn1jcWludk9ESmtpcGBMaDc9VzduZGc1aEdESDJQY1FCMDxINTV3Zm59bEBAUScpJ2N3amhWYHdzYHcnP3F3cGApJ2lkfGpwcVF8dWAnPyd2bGtiaWBabHFgaCcpJ2BrZGdpYFVpZGZgbWppYWB3dic%2FcXdwYHgl')
+
 }
 
