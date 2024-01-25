31cd33134dd1135f754090b4ab6bb2be86e036f9
Amrit-Raj12 <67092088+Amrit-Raj12@users.noreply.github.com>
2024-01-25 11:47:28 +0530

    Feature/filter (#79)
    * Dev - added filter handlers
    
    * Dev - added filter handler
    
    * Dev - intgrated filter post api

--- src/pages/category/[category].js
+++ src/pages/category/[category].js
@@ -2,6 +2,7 @@ import React, { Suspense, lazy, useEffect } from "react"
 import { useState } from "react"
 import axios from "axios"
 import Loader from "../../components/loading/loading"
+import { useRouter } from "next/router"
 
 const Cateogry = lazy(() => import('../../features/category/index'))
 function valuetext(value) {
@@ -13,6 +14,7 @@ export async function getServerSideProps(context) {
   try {
     const response = await axios.get(`https://auth-task-app.up.railway.app/api/products/search/${category}`);
     data = response?.data
+    // console.log('data', data)
   } catch (error) {
     console.error(error)
   }
@@ -31,14 +33,59 @@ const Filter = ({ data }) => {
   const [value, setValue] = React.useState([1000, 8000]);
   const [products, setProducts] = useState(data)
   const [loading, setLoading] = useState(false)
+  const [loadingProduct, setLoadingProduct] = useState(false)
   const [grid, setGrid] = useState(false)
+  const [filterData, setFilterData] = useState()
+
+  const [selectedBrands, setSelectedBrands] = useState([])
+
+  const [uniqueBrands, setUniqueBrand] = useState([])
+
+  const router = useRouter();
+
+  const category = router.query.category;
+
+  let brandData = []
+
+  const handleFilters = ({category, filterName, data}) => {
+    // console.log('adsadad', category, filterName)
+    setLoadingProduct(true);
+    setTimeout(() => {
+      axios.post(`https://auth-task-app.up.railway.app/api/products/${category}/filterby/${filterName}`, data)
+      .then((res) => {
+          if (res?.data?.success) {
+            setProducts(res?.data?.flData);
+            setLoadingProduct(false)
+          } else {
+            setLoadingProduct(true);
+          }
+      })
+      .catch((err) =>{setLoadingProduct(true); console.log('Error:', err.message)})
+    }, 1000);
+   
+  }
+
   const handleChange = (event, newValue) => {
+    setFilterData({
+      name: event.target.name,
+      value: event.target.value
+    })
+    if(event.target.name === 'price') {
+      handleFilters({
+        category:category,
+        filterName: event.target.name,
+        data: {
+          min: 0,
+          max: event.target.value
+        }
+      })
+    }
+   
     const filterPrice = products.filter((val) => {
       return val.price < newValue
     })
     setProducts(filterPrice)
     setLoading(!loading)
-    console.log(filterPrice, newValue)
   };
   const handleLowToHigh = () => {
     const sortedItems = products.sort((a, b) => a.price - b.price);
@@ -60,6 +107,19 @@ const Filter = ({ data }) => {
 
   }, [loading])
 
+  useEffect(() => {
+    setUniqueBrand([...new Set(products.map(product => product.brand))]);
+  }, [])
+
+
+
+    // Extracting unique brand names
+    // const uniqueBrands = [...new Set(products.map(product => product.brand))];
+
+  // Finding the maximum price
+  // const maxPrice = products.reduce((max, product) => (product.price > max ? product.price : max), 0);
+
+
   return (
     <Suspense fallback={<Loader />}>
       <Cateogry
@@ -74,7 +134,17 @@ const Filter = ({ data }) => {
         setToggleRating={setToggleRating}
         handleHighToLow={handleHighToLow}
         handleChange={handleChange}
-        products={products} />
+        products={products}
+        grid={grid}
+        setGrid={setGrid}
+        uniqueBrands={uniqueBrands}
+        selectedBrands={selectedBrands}
+        setSelectedBrands={setSelectedBrands}
+        brandData={brandData}
+        handleFilters={handleFilters}
+        loadingProduct={loadingProduct}
+        // maxPrice={maxPrice}
+        />
     </Suspense>
 
 
